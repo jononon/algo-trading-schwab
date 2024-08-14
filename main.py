99eebf9e0abe599a50fbc42878db522b52454f3c
Jonathan Damico <jj_damico@yahoo.com>
2024-08-14 06:31:50 -0700

    Increase trailing stop percentage

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ logger.setLevel("INFO")
 
 client = RESTClient(api_key=get_secret("/algotrading/polygon/apikey"))
 
-TRAILING_STOP_PERCENTAGE = 2.5
+TRAILING_STOP_PERCENTAGE = 3.5
 
 def create_strategy():
     data = {
